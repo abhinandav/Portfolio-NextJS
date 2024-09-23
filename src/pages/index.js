79798d3b7fd7616fd54fbb352a33b3c978e668a1
abhinand <abhinandorg3@gmail.com>
2024-09-23 12:01:16 +0530

    change resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
               Explore my latest projects and articles, showcasing my expertise in React.js and web development.
             </p>
             <div className='flex items-center self-start mt-2 lg:self-center'>
-              <Link href="abhinand.pdf" target={"_blank"}
+              <Link href="/abhinand.pdf" target={"_blank"}
               className='flex items-center bg-dark text-light p-2.5 px-6
               rounded-lg text-lg font-semibold
               hover:bg-light hover:text-black
